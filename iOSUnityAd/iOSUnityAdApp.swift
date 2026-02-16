28d4230ddb714c8494df1d0d789e906b8ea225bd
Rick <rcsw.devel@gmail.com>
2026-02-15 23:10:33 -0800

    Initial Commit

---
+++ iOSUnityAd/iOSUnityAdApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSUnityAdApp.swift
+//  iOSUnityAd
+//
+//  Created by Rick Cheng on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSUnityAdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOSUnityAdTests/iOSUnityAdTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSUnityAdTests.swift
+//  iOSUnityAdTests
+//
+//  Created by Rick Cheng on 2/15/26.
+//
+
+import Testing
+@testable import iOSUnityAd
+
+struct iOSUnityAdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
